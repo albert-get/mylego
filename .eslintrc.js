803c195b52383c7038ed6b7440d226f1982e48cd
章焱槟 <wxzhangyanbing@gfpartner.com.cn>
2021-08-09 11:32:41 +0800

    fix:eslint和prettier同时用，并在commit之前检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,11 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const a = {
+    a: '1',
+    b: '2',
+    c: {
+        n: [
+            '1',
+            '2'
+        ]
+    }
+}
