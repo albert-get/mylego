7ebcb955fe606894a1896e81f629ff1447a72081
章焱槟 <wxzhangyanbing@gfpartner.com.cn>
2021-08-09 14:20:19 +0800

    fix:husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,9 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'plugin:prettier/recommended',
-    'eslint:recommended',
     '@vue/typescript/recommended',
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
