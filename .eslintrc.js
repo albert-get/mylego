15e024645e6cd21e2b02ba5f9de39775d247535c
章焱槟 <wxzhangyanbing@gfpartner.com.cn>
2021-08-09 13:32:05 +0800

    fix:提交检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,11 @@ module.exports = {
     'plugin:prettier/recommended',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'semi': ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-const a = {
-    a: '1',
-    b: '2',
-    c: {
-        n: [
-            '1',
-            '2'
+const a ={
+a:'1',
+            b:'2',
+    c:{
+        n:[
+'1',
+                         '2'
         ]
     }
+    
 }
