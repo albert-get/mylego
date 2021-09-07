8b768693a8bf28138ea5edcf4ab184e3c0affcdf
harry <harry@harrydeMacBook-Pro.local>
2021-09-07 22:18:58 +0800

    add:增加路由

---
+++ src/routes/index.ts
@@ -0,0 +1,41 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from '../views/Home.vue'
+import Index from '../views/Index.vue'
+import TemplateDetail from '../views/TemplateDetail.vue'
+import Works from '../views/works.vue'
+
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [{
+        path: '/',
+        name: 'index',
+        component: Index,
+        children: [{
+            path: '',
+            name: 'home',
+            component: Home,
+            meta: { title: '欢迎来到慕课乐高' },
+        },{
+            path: '/template/:id',
+            name: 'template',
+            component: TemplateDetail,
+            meta: { title: '模版详情' }
+        },{
+            path: 'works',
+            name: 'works',
+            component: Works,
+            meta: { title: '我的作品', requireLogin: true }
+        }]
+    },{
+        path: '/editor/:id',
+        name: 'editor',
+        component: () => import( '../views/editor.vue' ),
+        meta: { title: '编辑我的设计', requireLogin: true }
+    },{
+        path: '/login',
+        name: 'login',
+        component: () => import( '../views/Login.vue' ),
+        meta: {title: '登录到慕课乐高', disableLoading: true }
+    }]
+})
