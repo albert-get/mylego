ce041ee5ad8f2986f065335c7293fbe631cdfa67
harry <harry@harrydeMacBook-Pro.local>
2021-10-07 18:29:05 +0800

    feat:footer文字和样式

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -10,8 +10,43 @@
                 <router-view></router-view>
             </a-layout-content>
         </a-layout>
-        <a-layout-footer>
-            © 慕课网（imooc.com）版权所有 | 津ICP备20000929号-2
+        <a-layout-footer class="index-footer">
+            <div class="friend-link">
+                <div class="item">
+                    <span class="title">慕课乐高</span>
+                    <a class="a">购买课程</a>
+                    <a class="a">作业和打卡</a>
+                    <a class="a">开源仓库</a>
+                    <a class="a">帮助</a>
+                </div>
+                <div class="item">
+                    <span class="title">设计制作帮助</span>
+                    <a class="a">购买课程</a>
+                    <a class="a">作业和打卡</a>
+                    <a class="a">开源仓库</a>
+                    <a class="a">帮助</a>
+                </div>
+                <div class="item">
+                    <span class="title">审核问题</span>
+                    <a class="a">购买课程</a>
+                    <a class="a">作业和打卡</a>
+                    <a class="a">开源仓库</a>
+                    <a class="a">帮助</a>
+                </div>
+                <div class="item">
+                    <span class="title">联系我们</span>
+                    <a class="a">购买课程</a>
+                    <a class="a">作业和打卡</a>
+                    <a class="a">开源仓库</a>
+                    <a class="a">帮助</a>
+                </div>
+            </div>
+            <div class="footer-line"></div>
+            <div class="footer-word">
+                <span>
+                    <span class="a-left">© 慕课网（imooc.com）版权所有 | </span><a class="a-right">津ICP备20000929号-2</a>
+                </span>
+            </div>
         </a-layout-footer>
     </div>
 </template>
@@ -50,6 +85,49 @@ export default defineComponent({
 .home-layout{
     position: relative;
     top: -70px;
+}
+.index-footer{
+    background-color: #333;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 30px;
+}
+.index-footer .friend-link {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+.friend-link .item {
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.item .title {
+    color: #fff;
+    font-size: 19px;
+    padding-bottom: 10px;
+}
+.item .a {
+    color: #666;
+    font-size: 16px;
+    padding-bottom: 10px;
+}
+.footer-word .a-left {
+    color: #fff;
+    font-size: 16px;
+}
+.footer-word .a-right {
+    font-size: 16px;
+}
+.footer-line {
+    width: 80%;
+    height: 1px;
+    background: #666;
+    margin-bottom: 20px;
 }
 </style>
 
